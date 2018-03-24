756609ac3074599060290c8c3357207f1a0c5be5
Joao-Munhoz <joaovictor.munhoz95@gmail.com>
2018-03-23 21:51:38 -0300

    Modificado

--- src/ccc_lib.c
+++ src/ccc_lib.c
@@ -2,15 +2,40 @@
 #include "ccc_lib.h"
 
 void camelcase_to_underline(char *input, char *output) {
+	
+	int i = 0, j = 0;	
+	while(input[i] != '\0'){
+		if(i == 0)
+			output[j] = input[i] + 32; 
+
+		else if(input[i] >= 65 && input[i] <= 90){
+			output[j] = '_';
+			j++;
+			output[j] = input[i] + 32;	
+		} 
+		else 
+			output[j] = input[i];
+		i++;
+		j++;
+	}
 
   return;
 }
 
 void underline_to_camelcase(char *input, char *output) {
+	
+	int i = 0, j = 0;
+	while(input[i] != '\0'){
+		if(i == 0)
+			output[j] = input[i] - 32; 
 
+		else if(input[i] == '_'){
+			output[j] = input[++i] - 32;
+		}
+		else
+			output[j] = input[i];
+		i++;
+		j++;
+	}
   return;
 }
-
-
-
-
